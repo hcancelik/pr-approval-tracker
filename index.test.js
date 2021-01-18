a3e8b22b6c9221d8feb99b7fd57335907593f1fe
Can Celik <celik@me.com>
2021-01-18 23:12:29 +0300

    update tests

--- index.test.js
+++ index.test.js
@@ -1,5 +1,11 @@
+let token;
+
+beforeAll(() => {
+  token = process.env.TEST_KEY;
+})
+
 test("true is true", async () => {
-  console.log(`github token: ${process.env.TEST_KEY}`);
+
 
   expect(true).toBeTruthy();
 });

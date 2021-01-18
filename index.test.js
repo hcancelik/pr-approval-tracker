49375fad19604c813277f201fafe0eec13a8ce44
Can Celik <celik@me.com>
2021-01-18 22:52:30 +0300

    Test token in tests

--- index.test.js
+++ index.test.js
@@ -1,3 +1,9 @@
+const core = require("@actions/core");
+
 test("true is true", async () => {
+  const githubToken = core.getInput("github_token");
+
+  core.info(`github token: ${githubToken}`);
+
   expect(true).toBeTruthy();
 });
